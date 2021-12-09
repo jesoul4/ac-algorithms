c5665fdf9216617dd13216f0470807c8258c8342
Jesoul <jesoul@anniecannons.com>
2021-12-09 14:01:48 -0500

    two methods

--- isPrime.js
+++ isPrime.js
@@ -32,5 +32,32 @@ console.log(result) // false
 **/
 
 function isPrime(num) {
-	// Your code
-}
+    // Your code
+    function isPrime(n) {
+        if (n === 1) {
+            return false;
+        } else if (n === 2) {
+            return true;
+        } else {
+            for (let i = 2; i < n; i++) {
+                if (n % i === 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    //     console.log(isPrime(1));
+    //     console.log(isPrime(2));
+    //     console.log(isPrime(3));
+    //     console.log(isPrime(4));
+    //     console.log(isPrime(29));
+    //     console.log(isPrime(51));
+
+    // for (let i = 2; i < n; i++) {
+    //     if (n % i === 0) {
+    //         return false;
+    //     }
+    // }
+    // return n > 1
